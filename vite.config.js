112d9efafcea4ba7da49084ffb4ae9f5ec5532b2
paulafc30 <paulafc30@gmail.com>
2025-09-10 01:34:19 +0200

    Fixing vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
 import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-
 export default defineConfig(({ mode }) => {
-
     // Carga las variables de entorno del archivo .env
     const env = loadEnv(mode, process.cwd());
 
     return {
-        base: '/build/',
         plugins: [
             laravel({
                 input: ['resources/css/app.css', 'resources/js/app.js'],
                 refresh: true,
             }),
         ],
-        // Exponer la variable al front-end
+        // Expone la variable al front-end
         define: {
             'import.meta.env.VITE_APP_URL': JSON.stringify(env.APP_URL),
-        }
+        },
+        // Usa la URL de la aplicación como base
+        base: env.APP_URL + '/build/'
     };
 });
