ce7e59cbb863f4440f6e7928141e4626f7d76a17
Paula <paulafc30@gmail.com>
2026-07-12 19:34:56 +0200

    add folders.js and files.js as Vite entry points and add password show/hide toggle on login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,10 +26,19 @@
 
                 <div class="mt-4">
                     <x-input-label for="password" :value="__('Password')" />
-                    <x-text-input id="password" class="block mt-1 w-full"
-                                  type="password"
-                                  name="password"
-                                  required autocomplete="current-password" />
+                    <div class="relative mt-1">
+                        <x-text-input id="password" class="block w-full pr-10"
+                                      type="password"
+                                      name="password"
+                                      required autocomplete="current-password" />
+                        <button type="button" onclick="togglePassword('password', this)"
+                                class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 hover:text-gray-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                            </svg>
+                        </button>
+                    </div>
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
@@ -74,6 +83,15 @@
             </div>
         </x-guest-layout>
     @endif
+
+    <script>
+        function togglePassword(id, btn) {
+            const input = document.getElementById(id);
+            const isPassword = input.type === 'password';
+            input.type = isPassword ? 'text' : 'password';
+            btn.querySelector('svg').style.opacity = isPassword ? '0.5' : '1';
+        }
+    </script>
 </body>
 
 </html>
